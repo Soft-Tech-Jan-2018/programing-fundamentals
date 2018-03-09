93d03d431b11cefd1d2831c67cf57b372ea97122
bubalik <bubalik@gmail.com>
2018-03-10 00:54:45 +0200

    added problem one solution

---
+++ intro-and-basic-syntax/exercise/Debit Card Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int a3 = int.Parse(Console.ReadLine());
+            int a4 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a1:d4} {a2:d4} {a3:d4} {a4:d4}");
+        }
+    }
+}
