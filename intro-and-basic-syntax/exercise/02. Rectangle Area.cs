4a7ff9242ab34dc1acb43f068b6e2f63d70b3bb0
igril <igrilkul@hotmail.com>
2018-03-18 17:49:18 +0200

    Added 2nd exercise

---
+++ intro-and-basic-syntax/exercise/02. Rectangle Area.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Debit
+{
+    class Program
+    {
+        static void Main(String[] Args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a * b:F2}");
+        }
+    }
+}
