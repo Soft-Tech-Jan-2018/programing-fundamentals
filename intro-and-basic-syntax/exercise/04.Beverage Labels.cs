cb2e3c97361dc2fab676bfe788465de9dd8454da
bubalik <bubalik@gmail.com>
2018-03-19 00:37:33 +0200

    Changed variables name in 2-4

--- intro-and-basic-syntax/exercise/04.Beverage Labels.cs
+++ intro-and-basic-syntax/exercise/04.Beverage Labels.cs
@@ -6,13 +6,13 @@ namespace Debit
         static void Main(String[] Args)
         {
             //read from the console a food product name, volume, energy content per 100ml and sugar content per 100ml.
-            string a = Console.ReadLine();
-            int vol = int.Parse(Console.ReadLine());
-            double kcal = double.Parse(Console.ReadLine());
-            double g = double.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            double energyContent = double.Parse(Console.ReadLine());
+            double sugarContent = double.Parse(Console.ReadLine());
 
             //Calculate the energy and sugar content for the given volume and print them on the console.
-            Console.WriteLine($"{vol}ml {a}:\n{vol * kcal / 100}kcal, {vol * g / 100}g sugars");
+            Console.WriteLine($"{volume}ml {name}:\n{volume * energyContent / 100}kcal, {volume * sugarContent / 100}g sugars");
         }
     }
 }
