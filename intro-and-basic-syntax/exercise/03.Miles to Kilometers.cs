7cb303994db48008393e9c2cc7d64a496ae30817
igril <igrilkul@hotmail.com>
2018-03-18 21:10:22 +0200

    Added comments to 1-3-5

--- intro-and-basic-syntax/exercise/03.Miles to Kilometers.cs
+++ intro-and-basic-syntax/exercise/03.Miles to Kilometers.cs
@@ -5,9 +5,9 @@ namespace Debit
     {
         static void Main(String[] Args)
         {
+			//We get the miles from the console input
             double a = double.Parse(Console.ReadLine());
-
-
+            //We convert the miles into kilometers and print the outcome
             Console.WriteLine($"{a * 1.60934:F2}");
         }
     }
--- intro-and-basic-syntax/exercise/05.Character Stats.cs
+++ intro-and-basic-syntax/exercise/05.Character Stats.cs
@@ -5,19 +5,20 @@ namespace Debit
     {
         static void Main(String[] Args)
         {
+            //We get the character name and stats via console input in the next 5 lines
             string a = Console.ReadLine();
-            int curHP = int.Parse(Console.ReadLine());
+            int curHP = int.Parse(Console.ReadLine()); 
             int maxHP = int.Parse(Console.ReadLine());
             int curMP = int.Parse(Console.ReadLine());
             int maxMP = int.Parse(Console.ReadLine());
 
-            string hp = new string('|', curHP);
-            string mp = new string('|', curMP);
-            string hpd = new string('.', (maxHP - curHP));
-            string mpd = new string('.', (maxMP - curMP));
+            string hp = new string('|', curHP); //string represents bars of remaining Health
+            string mp = new string('|', curMP); //string represents bars of remaining Energy
+            string hpd = new string('.', (maxHP - curHP)); //string represents depleted Health
+            string mpd = new string('.', (maxMP - curMP)); //string represents depleted Energy
 
 
-
+            //We print the character's name and stats
             Console.WriteLine($"Name: {a}\nHealth: |{hp}{hpd}|\nEnergy: |{mp}{mpd}|");
         }
     }
