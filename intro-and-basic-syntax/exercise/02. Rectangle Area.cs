b7ea247a1b63cc9ab3b6bde0d7151646f6686478
bubalik <bubalik@gmail.com>
2018-03-19 00:00:07 +0200

    Added comments to 2-4

--- intro-and-basic-syntax/exercise/02. Rectangle Area.cs
+++ intro-and-basic-syntax/exercise/02. Rectangle Area.cs
@@ -5,9 +5,10 @@ namespace Debit
     {
         static void Main(String[] Args)
         {
+            //read from the console width and height of a rectangle as floating point numbers
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
-
+            //priny on the console area of the rectangle formatted to the 2nd character after the decimal point
             Console.WriteLine($"{a * b:F2}");
         }
     }
